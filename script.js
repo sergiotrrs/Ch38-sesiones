28584c667ef7c9fb4a2dfb1d083cf75d808d2738
Sergio Torres <sergio.torres@idr.edu.mx>
2024-02-19 12:59:47 -0600

    ci: funcion saludar a persona

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Hola Ch38");
+
+// Función que envie un saludo con alert.
+function enviarSaludo(){
+    alert('¡Hola Ch38!');
+}
+
+/**
+ * Pedir el nombre de la persona usando promt()
+ * Mostrar un saludo y nombre de la persona con alert()
+ */
+function enviarSaludoAPersona() {
+    let message = prompt("Hola usuario dime tu nombre");
+    alert(`Hola ${message}`);
+}
