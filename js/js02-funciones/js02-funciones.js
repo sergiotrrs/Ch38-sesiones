43a518940f4f83b8f6a71fd9a68ffe17fb15d873
Sergio Torres <sergio.torres@idr.edu.mx>
2024-03-04 16:37:52 -0600

    ci: pase por valor y por referencia

--- js/js02-funciones/js02-funciones.js
+++ js/js02-funciones/js02-funciones.js
@@ -46,3 +46,74 @@ myScopeFunction();
 // console.log( myVarFunctionVar );
 // console.log( myVarFunctionLet );
 // console.log( myVarFunctionConst );
+
+// =======================================================================
+
+/*
+https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Functions
+
+Función: Conjunto de instrucciones que realiza una tarea o calcula un valor.
+  Código que se puede reutilizar.
+
+
+  ----------- Funciones declaradas --------------
+    (function declaration, function statement)
+Una característica de las funciones declaradas es que tien hoisting.
+
+sintaxis:
+    function nombreFuncionCamelCase ( parámetros ){
+        //cuerpo de la función
+        instrucciones;
+    }
+
+hoisting: comportamiento en JS que permite a las declaración de variables(var)
+o funciones se eleven al comienzo de su ámbito antes de que se ejecute el código.
+
+*/
+console.log( greeting() );
+
+function greeting() {
+    return "Hola Ch38";
+}
+
+
+/*
+ ------------ Funciones expresadas -----------------------
+            (function expressions)
+Son funciones declaradas dentro de la asignación de una variable.
+Estas funciones pueden ser anónimas ( no tienen nombre ).
+Las funciones expresadas no tiene hoisting, porque no se
+carga en memoria hasta que se utilice.
+
+sintaxis:
+    const nombreVariable = function nombreFuncion (parámetros){
+        instrucciones;
+    };
+*/
+
+// console.log(  sum(10, 20) ); // Cannot access 'sum' before initialization
+
+const sum = function ( a , b){
+    return a + b;
+};
+
+console.log(  sum(3, 15) ); // 18
+
+// Pase por valor y pase por referencia =====================
+// en los datos primitivos el pase es por valor
+let age = 19;
+let newAge = age; // 19
+age = 40;
+console.log( newAge ); // 19
+
+// en los objetos el pase de datos es por referencia
+let myArray = [1,2,3];
+let numbers = myArray;
+myArray.push(4); // agregamos un elemento al final del arreglo [1,2,3, 4]
+console.log( numbers ); // [1,2,3,4];
+
+// Clonar un array, para que sean objetos distintos.
+// slice: crea una copia superficial del array original, desde el principio hasta el final.
+let cloneNumbers = myArray.slice();
+myArray.push(5);  // [1,2,3,4,5];
+console.log( cloneNumbers ); // // [1,2,3,4];
