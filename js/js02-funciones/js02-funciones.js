3f6bce7f8d46c9f2b46eb46b4df98bf70d1b2145
Sergio Torres <sergio.torres@idr.edu.mx>
2024-03-05 13:50:51 -0600

    excercise: resultado ejercicio 1

--- js/js02-funciones/js02-funciones.js
+++ js/js02-funciones/js02-funciones.js
@@ -294,4 +294,17 @@ const comparacionDesendente = (a,b) => b-a;
 console.log( [4,90,2,67].sort(  (a,b) => b - a) ); // orden descendente [ 90, 67, 4, 2]
 
 
+// Solución ejercicio 1.
+let arr = ["This", "is", "a", "sentence."];
 
+function printOutString(arr) {
+    let sentence = "";
+    for (let wordIndex = 0; wordIndex < arr.length; wordIndex++) {
+        sentence += " "+ arr[wordIndex]; // sentence = sentence + " "+ arr[wordIndex];
+    }
+    return sentence;
+  }
+  console.log(printOutString(arr)); // " This is a sentence."
+  console.log( arr.join(" ")  );
+
+  
