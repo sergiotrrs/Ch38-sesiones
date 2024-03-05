e8a836686da5b6e855efa4f9e0c837324af04cd5
Sergio Torres <sergio.torres@idr.edu.mx>
2024-03-05 16:50:01 -0600

    feat: js03 condicionales

---
+++ js/js03-condicionales/js03-condicionales.js
@@ -0,0 +1,48 @@
+console.log("Sesion JS03 condicionales");
+
+
+// ----------------- Condicional if-else --------------------
+/*
+ Ejecuta una sentencia si una condición específica es
+ evaluada como verdadera.
+
+ Sintaxis:
+    if ( condición ) sentencia;
+
+    if ( condición ) {
+        sentencias;
+    }
+
+    if ( condición ) sentencia;
+    else sentencia_si_condición_es_falsa;
+
+    if ( condición ) {
+        sentencias;
+    }
+    else {
+        sentencias;
+    }
+
+    if (condición) sentencia;
+    else if (condicion 2) sentencia;
+         else sentencia_si_condicion2_es_falsa;
+
+
+    if ( condicion1 ) sentencias;
+    else if ( condicion2 ) sentencia;
+    else if ( condicion3 ) sentencia;
+    else if ( condicion4 ) sentencia;
+     .....
+    else if ( condicionN ) sentencia;
+    else condición;   
+
+*/
+
+const temperatura = 25;
+let mensaje = "";
+
+if( temperatura === 22 ){
+    mensaje = `Temperatura de ${temperatura} es ideal`;
+}
+
+console.log( mensaje ); // ""
