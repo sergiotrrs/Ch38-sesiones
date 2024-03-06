74cc2f1725c56739aa952ae84ea4054a4f2efa80
Sergio Torres <sergio.torres@idr.edu.mx>
2024-03-06 12:16:15 -0600

    ci: metodos del arreglo push, pop, shift, unshift

--- js/js04-arrays_ciclos/js04-arrays.js
+++ js/js04-arrays_ciclos/js04-arrays.js
@@ -1,2 +1,23 @@
 console.log("Sesion JS04 arrays");
 
+const nombre = [];
+
+// Agregar elementos al final del arreglo
+nombre.push("Sergh");
+nombre.push("Emma", "Axel");
+
+console.log( nombre ); // ['Sergh', 'Emma', 'Axel']
+
+// Agregar elementos al inicio del arreglo
+nombre.unshift("Dan");
+nombre.unshift("Bastian", "Ara");
+
+console.log( nombre ); // ['Bastian', 'Ara', 'Dan', 'Sergh', 'Emma', 'Axel']
+
+// Eliminar el último elemento
+nombre.pop();
+console.log( nombre ); // ['Bastian', 'Ara', 'Dan', 'Sergh', 'Emma']
+
+// Eliminar el primer elemento
+nombre.shift();
+console.log( nombre ); // [ 'Ara', 'Dan', 'Sergh', 'Emma']
