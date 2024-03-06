fa9241c5986ef4ea1075855aa67a8d01d140755b
Sergio Torres <sergio.torres@idr.edu.mx>
2024-03-06 12:35:21 -0600

    ci: metodos de array

--- js/js04-arrays_ciclos/js04-arrays.js
+++ js/js04-arrays_ciclos/js04-arrays.js
@@ -21,3 +21,28 @@ console.log( nombre ); // ['Bastian', 'Ara', 'Dan', 'Sergh', 'Emma']
 // Eliminar el primer elemento
 nombre.shift();
 console.log( nombre ); // [ 'Ara', 'Dan', 'Sergh', 'Emma']
+
+// Buscar elementos dentro del arreglo
+console.log(  nombre.indexOf("Frida")   ); // si el elemento no existe retorna -1
+console.log(  nombre.indexOf("Frida")!= -1 ? "Existe" : "No Existe"   ); // No existe
+
+console.log(  nombre.indexOf("Emma")   ); // 3
+console.log(  nombre.indexOf("Emma")!= -1 ? "Existe" : "No Existe"   ); // Existe
+
+// Vamos a agregar a Cristian al inicio y al final
+nombre.push("Cristian");
+nombre.unshift("Cristian");
+console.log( nombre ); // ['Cristian', 'Ara', 'Dan', 'Sergh', 'Emma', 'Cristian']
+
+// buscar a Cristian si existe y mostrar el índice
+// indexOf entrega el indice del primer elemento que encuentre.
+console.log(nombre.indexOf("Cristian")); // 0
+// Podemos indicar a partir de que índice deseo buscar
+console.log(nombre.indexOf("Cristian", 1 )); // 5
+
+// Crear una copia (clonarlo) de un arreglo
+const copyOfNames = nombre.slice();
+
+
+
+
