4bdae53d2b8375e14b65c6ce9ae392b7a4bb15cc
Sergio Torres <sergio.torres@idr.edu.mx>
2024-03-08 12:06:29 -0600

    ci: operadores unarios

--- js/js05-operadores/js05-operadores.js
+++ js/js05-operadores/js05-operadores.js
@@ -1,1 +1,142 @@
 console.log("Js-05 operadores");
+
+// ---------------- Opéradores aritméticos ----------------
+/* 
+   adición +
+   multiplicación *
+   substracción -
+   división /
+   resto %
+   exponencial **  - Math.pow(2, 3)   2**3
+*/
+
+
+// ---- Precedencia de operadores -------------------------
+// https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
+
+console.log(  3 * 2 / 2  ); // 3
+console.log(  3 / 2 * 2  ); // 3
+console.log(  2 * 4 / 2**3 ); // 1
+console.log(  3 ** 2 ** 3  ); // 6561
+
+// ------- Operadores de asignación ----------------
+/*
+ Asigna un valor a su operador izquierdo basándose en el valor
+ de su operando derecho:
+
+ asignación =
+ 
+ Operadores abreviados ( compuestos ):
+  op1 += op2  -> op1 = op1 + op2
+  op1 -= op2  -> op1 = op1 - op2
+  op1 *= op2  -> op1 = op1 * op2
+
+*/
+
+let fraseBurro = "Ahí te voy San Pedro";
+
+// fraseBurro = fraseBurro + " Ahora somo Sexys!";
+// usando operadores compuestos
+fraseBurro += " Ahora somo Sexys!";
+console.log( fraseBurro );
+
+// Ejercicio
+let animalesEnPantano = 3;
+console.log(  animalesEnPantano += 3 * (3 ** 3) + 10 ); // 94
+
+// -------------- Operadores unarios ------------------
+// Solo actuan sobre un operando
+// Negación unaria
+let numero = 3;
+let dinero = - numero; // -3
+console.log( numero, dinero ); // 3 , -3
+
+// suma unaria
+let pago = + dinero; // -3
+console.log( pago ); // -3
+
+let interes = "5.23"; // string
+console.log( interes + 5 ); // '5.235'
+console.log( interes - 5 ); // conversión implícita de "intereses") 5, r: 0.23
+
+console.log(  parseFloat(interes) + 5 ); // '10.23'
+console.log(  +interes + 5 ); // '10.23'
+
+console.log( interes += 5 + + "3" ); // 5.238
+
+
+// Operador de incremento y decremento en unidad.
+/*
+  Operador de pre-incremento y pre-decremento
+    ++ valor
+    -- valor
+
+  Operador de post-incremento y post-decremento
+    valor ++
+    valor --
+
+*/
+
+let number = 10;
+++ number; // number = number + 1; primero lo incrementa y se entrega su valor
+console.log( number ); // 11
+number ++; // number; number = number + 1; primeo entrega su valor y luego incrementa
+console.log( number ); // 12
+
+number = 20;
+console.log( number   ); // 20
+console.log( ++number ); // 21
+console.log( number++ ); //21
+console.log( number   ); // 22
+
+number = 100;
+console.log(  number += 50 + ++number ); // 251
+console.log( number ); // 251
+
+
+number = 100;
+let nuevaVariable
+console.log(  nuevaVariable = 50 + number++ ); // 150
+console.log( number ); // 101
+
+// ===============================
+
+let x = 3;  
+let y = ++x; // y:4 x:4          ( ++x   -> x = x +1)
+
+console.log(`x :${ x++ } y:${ ++y }`); //  x: 4 (sig. 5)  y: 5
+console.log(`x :${ x } y:${ y }`); // 5, 5
+
+// ==================
+number = 10;
+let otroNumber = 15;
+let res = 0;
+res += 10 * number++ * otroNumber++;
+
+console.log(res, number, otroNumber); // 1500, 11, 16
+
+
+// let a = b = 0; b es declarada con var
+let a = 0, b = 0;
+
+for(  ; a < 3 ; b = a++ ){ 
+    console.log( a, b);
+    // iteracion    a    b
+    //   1          0    0
+    //   2          1    0
+    //   3          2    1
+
+}
+console.log( a, b); // 3  , 2
+
+
+a = 0, b = 0;
+for(  ; a < 3 ; b = ++a ){ 
+    console.log( a, b);
+    // iteracion    a    b
+    //   1          0    0         
+    //   2          1    1          
+    //   3          2    2        
+
+}
+console.log( a, b); // 3 , 3
