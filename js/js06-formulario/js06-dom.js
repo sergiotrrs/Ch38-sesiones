845ff8030ea66e37493c21f153fa2024e287ad67
Sergio Torres <sergio.torres@idr.edu.mx>
2024-03-11 13:55:20 -0600

    ci: addevenListener

--- js/js06-formulario/js06-dom.js
+++ js/js06-formulario/js06-dom.js
@@ -1,1 +1,18 @@
 console.log("Js06 formulario");
+
+// Obtenemos la referencia del formulario.
+// const register = document.querySelector("form");
+//const register = document.getElementById("register-form");
+const register = document.forms["register-form"];
+
+/**
+ *  AddEventListener permite agregar funciones que se ejecutarán
+ *  en respuesta a un evento específico que ocurre aun elemento del DOM.
+ * 
+ *  register.addEventListener( type  , fncCallback );
+ * 
+ *  addEvenListener podemos manejar interacciones del usuario como clics,
+ * cambios de teclado, movimientos del mouse, entre otros.
+ */
+register.addEventListener( type  , fncCallback );
+
