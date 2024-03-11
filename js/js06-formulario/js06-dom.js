58eec071e9d0735467222aace670dbf8ed3bdaef
Sergio Torres <sergio.torres@idr.edu.mx>
2024-03-11 14:29:16 -0600

    ci: leer formulario

--- js/js06-formulario/js06-dom.js
+++ js/js06-formulario/js06-dom.js
@@ -14,5 +14,39 @@ const register = document.forms["register-form"];
  *  addEvenListener podemos manejar interacciones del usuario como clics,
  * cambios de teclado, movimientos del mouse, entre otros.
  */
-register.addEventListener( type  , fncCallback );
+register.addEventListener( "submit"  , ( e )=>{
+   e.preventDefault();
+   console.log("Estoy dentro del evento submit");
+   
+   // leer los inputs del formulario
+   const newUser = { 
+     email: register.elements["inputEmail"].value,
+     password: register.elements["inputPassword"].value,
+     address: register.elements["inputAddress"].value,
+     checkbox: register.elements["gridCheck"].checked
+     };
+
+     console.table( newUser );
+
+    // Verificar los campos
+       // Si los campos son adecuados, enviar al servidor
+       // Si no son adecuados, enviar mensaje de error
+
+} );
+
+
+
+
+
+const handleButton = ( element, e ) =>{
+    /*
+      e->event : representa el objeto de evento que se genera cuando ocurre
+       una interacción del usuario.
+    */
+    console.log( element );
+    console.log( e );
+    console.log("He entrado en la función del botón");
+};
+
+
 
