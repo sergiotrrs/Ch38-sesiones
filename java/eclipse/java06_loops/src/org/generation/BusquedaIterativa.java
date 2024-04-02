734378a4f3ef5e05297b739995ccf2500c6857d6
Sergio Torres <sergio.torres@idr.edu.mx>
2024-04-02 14:15:21 -0600

    exercise: busqueda iterativa

---
+++ java/eclipse/java06_loops/src/org/generation/BusquedaIterativa.java
@@ -0,0 +1,20 @@
+package org.generation;
+
+public class BusquedaIterativa {
+	/*
+	 *  Buscar una pakabra dentro de una frase, 
+	 *  mostrar en consola: "Palabra encontrada" o "Palabra no encontra"
+	 *  
+	 *  ej. Frase: "Pepe Pecas Pica Papas"
+	 *      Palabra: "pecas"
+	 *      Respuesta: "Palabra encontrada"
+	 *      
+	 *  ej. Frase: "Pepe Pecas Pica Papas"
+	 *      Palabra: "mimir"
+	 *      Respuesta: "Palabra no encontrada"  
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ java/eclipse/java06_loops/src/org/generation/FizzBuzz.java
@@ -0,0 +1,30 @@
+package org.generation;
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+		/*
+		 *  Escribir un programa que muestre en pantalla los 
+		 *  números del 1 al 20, sustituyendo los múltiplos 
+		 *  de 3 por la palabra “fizz”, los múltiplos de 5 por 
+		 *  “buzz” y los múltiplos de ambos, es decir, los 
+		 *  múltiplos de 3 y 5 (o de 15), por la palabra 
+		 *  “fizzbuzz”.
+		 * 
+		 *   1
+		 *   2
+		 *   fizz
+		 *   4
+		 *   buzz
+		 *   fizz
+		 *   7
+		 *   8
+		 *   fizz
+		 *   
+		 *   
+		 * 
+		 */
+
+	}
+
+}
