d7601e28b91c3d5d78686c832a2068c33a621e5d
Sergio Torres <sergio.torres@idr.edu.mx>
2024-04-02 14:36:15 -0600

    exercice: resiltado fizzbuzz

--- java/eclipse/java06_loops/src/org/generation/BusquedaIterativa.java
+++ java/eclipse/java06_loops/src/org/generation/BusquedaIterativa.java
@@ -2,7 +2,7 @@ package org.generation;
 
 public class BusquedaIterativa {
 	/*
-	 *  Buscar una pakabra dentro de una frase, 
+	 *  Buscar una palabra dentro de una frase, 
 	 *  mostrar en consola: "Palabra encontrada" o "Palabra no encontra"
 	 *  
 	 *  ej. Frase: "Pepe Pecas Pica Papas"
@@ -14,7 +14,24 @@ public class BusquedaIterativa {
 	 *      Respuesta: "Palabra no encontrada"  
 	 */
 	public static void main(String[] args) {
+		String phrase = "Pepe pecas pica".toLowerCase(); // 15
+		String wordToSearch = "pepe".toLowerCase(); // 5
+		int maxIndexToSearch = phrase.length() - wordToSearch.length(); // 10
+		boolean foundIt = false;
+
+		search: for (int i = 0; i <= maxIndexToSearch; i++) {
+			for (int j = 0; j < wordToSearch.length(); j++) {
+				if (phrase.charAt(i + j) != wordToSearch.charAt(j)) {
+					continue search;
+				}
+			}
+			foundIt = true;
+			break search;
+		}
+
+		System.out.println("Palabra " + (foundIt ? "encontrada" : "no encontrada"));
 		
+		System.out.println(phrase.indexOf(wordToSearch));
 	}
 
 }
--- java/eclipse/java06_loops/src/org/generation/FizzBuzz.java
+++ java/eclipse/java06_loops/src/org/generation/FizzBuzz.java
@@ -4,27 +4,26 @@ public class FizzBuzz {
 
 	public static void main(String[] args) {
 		/*
-		 *  Escribir un programa que muestre en pantalla los 
-		 *  números del 1 al 20, sustituyendo los múltiplos 
-		 *  de 3 por la palabra “fizz”, los múltiplos de 5 por 
-		 *  “buzz” y los múltiplos de ambos, es decir, los 
-		 *  múltiplos de 3 y 5 (o de 15), por la palabra 
-		 *  “fizzbuzz”.
+		 * Escribir un programa que muestre en pantalla los números del 1 al 20,
+		 * sustituyendo los múltiplos de 3 por la palabra “fizz”, los múltiplos de 5 por
+		 * “buzz” y los múltiplos de ambos, es decir, los múltiplos de 3 y 5 (o de 15),
+		 * por la palabra “fizzbuzz”.
 		 * 
-		 *   1
-		 *   2
-		 *   fizz
-		 *   4
-		 *   buzz
-		 *   fizz
-		 *   7
-		 *   8
-		 *   fizz
-		 *   
-		 *   
+		 * 1 2 fizz 4 buzz fizz 7 8 fizz
+		 * 
+		 * 
 		 * 
 		 */
+		String frase;
+		for (int i = 1; i <= 100; i++) {
+			frase = "";
+			if( i % 3 == 0 ) frase = "Fizz";			
+			if( i % 5 == 0 ) frase += "Buzz";
+			
+			if( frase.equals("") ) System.out.println(i);
+			else System.out.println(frase);
 
+		}
 	}
 
 }
