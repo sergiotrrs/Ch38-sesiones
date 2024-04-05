e02046055d87c7f9fbfcd7f79ddaa49e4fe96e92
Sergio Torres <sergio.torres@idr.edu.mx>
2024-04-05 11:54:08 -0600

    feat: atm

---
+++ java/eclipse/java09_OOP/src/com/santander/Atm.java
@@ -0,0 +1,35 @@
+package com.santander;
+
+/**
+ *  Clase: Una clase es una componente fundamental de la programación
+ *  orientada a objeto(OOP: Object oriented Programming). 
+ *  Una clase es una plantilla que define el
+ *  comportamiento(métodos) y las propiedades(atributos) de los objetos
+ *  que se crean(instancian) a partir de ella. Una clase puede
+ *  considerarse como un plano o un molde a partir del cual se crean
+ *  los objetos( instanciar la clase).
+ *  
+ */
+public class Atm {
+	
+	// atributos de instancia (non-static fields)
+	int serialNumber;
+	String model;
+	double balance;
+	
+	
+	// atributos de clase (static fields)
+	
+	// métodos constructores
+	
+	// métodos de instancia (non-static methods)
+	double withdraw(double amout) {
+		balance = balance - amout; //  balance -= amout;
+		return balance;
+	}
+	
+	
+	// métodos estáticos(static methods)
+	
+
+}
---
+++ java/eclipse/java09_OOP/src/com/santander/AtmTest.java
@@ -0,0 +1,31 @@
+package com.santander;
+
+public class AtmTest {
+
+	public static void main(String[] args) {
+		
+		// Instanciar la clase ATM y asignarlo a una variable
+		Atm cajero01 = new Atm();
+		cajero01.serialNumber = 1;
+		cajero01.model = "T-800";
+		cajero01.balance = 450_000;
+		
+		// Instanciar la clase ATM y asignarle atributos
+		Atm cajero02 = new Atm();
+		cajero02.serialNumber = 2;
+		cajero02.model = "T-1000";
+		cajero02.balance = 600_000;
+		
+		// Monto total de dinero en la sucursal, cajero01 + cajero02
+		 double totalBalance = cajero01.balance + cajero02.balance;	      
+	     System.out.println("Total balance in the branch: " + totalBalance);
+	     System.out.printf("Total balance in the branch: $%,3.2f dollars%n", totalBalance);
+		
+		cajero01.withdraw( 400_000 );
+		// imprimir cuanto dinero queda en cajero01
+		 System.out.printf("Total balance in cajero01: $%,3.2f dollars%n", cajero01.balance);
+		
+		 
+	}
+
+}
